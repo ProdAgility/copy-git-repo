24c1c66fc5b64bcef214be4580a1760b7655cbf4
Roman Hood <contact@romanhood.com>
2021-10-12 20:37:22 -0400

    add worktree

--- copyRepo.js
+++ copyRepo.js
@@ -4,6 +4,7 @@ const { findAndRename, findAndReplace } = require('./utilities')
 module.exports = async (existing, copy) => {
   const execOpt = { encoding: 'utf8', stdio: 'inherit' }
   const tempDir = `${copy}-tmp`
+  execSync(`mkdir /tmp/worktree`, execOpt)
   execSync(`rm -rf ${tempDir} && cd /tmp && git clone --bare https://${process.env.GITHUB_TOKEN}@github.com/${existing}.git ${tempDir}`, execOpt)
   process.chdir(`/tmp/${tempDir}`)
   try {
@@ -12,7 +13,7 @@ module.exports = async (existing, copy) => {
   } catch (error) {
     console.log(error)
   }
-  execSync(`cd /tmp/${tempDir} && git -c user.name="${process.env.GIT_USER_NAME}" -c user.email="${process.env.GIT_EMAIL}" commit -am "updates"`, execOpt)
+  execSync(`cd /tmp/${tempDir} && git --work-tree=/tmp/worktree -c user.name="${process.env.GIT_USER_NAME}" -c user.email="${process.env.GIT_EMAIL}" commit -am "updates"`, execOpt)
   execSync(`cd /tmp/${tempDir} && git push --mirror https://${process.env.GITHUB_TOKEN}@github.com/${copy}.git`, execOpt)
   process.chdir(`/tmp/`)
   execSync(`rm -rf ${tempDir}`, execOpt)
