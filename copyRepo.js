fc531bc23a86f8c315b41f109d67e18cfb29fb06
Roman Hood <contact@romanhood.com>
2021-10-16 20:41:43 -0400

    updates

--- copyRepo.js
+++ copyRepo.js
@@ -3,18 +3,26 @@ const { findAndRename, findAndReplace } = require('./utilities')
 
 module.exports = async (existing, copy) => {
   const execOpt = { encoding: 'utf8', stdio: 'inherit' }
-  const tempDir = `${copy}-tmp`
-  execSync(`rm -rf /tmp/worktee && mkdir /tmp/worktree`, execOpt)
-  execSync(`rm -rf ${tempDir} && cd /tmp && git clone --bare https://${process.env.GITHUB_TOKEN}@github.com/${existing}.git ${tempDir}`, execOpt)
+  const tempDir = `repo`
+  process.chdir(`/tmp/`)
+  execSync(`rm -rf ${tempDir} && git clone https://${process.env.GITHUB_TOKEN}@github.com/${existing}.git ${tempDir}`, execOpt)
+
   process.chdir(`/tmp/${tempDir}`)
+  execSync(`rm -rf .git/`, execOpt)
   try {
-    await findAndReplace(existing, copy)
-    await findAndRename(existing, copy)
+    await findAndReplace("copyRepo", "copyGithubRepo")
+    await findAndReplace("copy-git-repo", "copy-github-repo")
+    await findAndRename("copyRepo", "copyGithubRepo")
   } catch (error) {
     console.log(error)
   }
-  execSync(`cd /tmp/${tempDir} && git --work-tree=/tmp/worktree -c user.name="${process.env.GIT_USER_NAME}" -c user.email="${process.env.GIT_EMAIL}" commit -am "updates"`, execOpt)
-  execSync(`cd /tmp/${tempDir} && git push --mirror https://${process.env.GITHUB_TOKEN}@github.com/${copy}.git`, execOpt)
+  execSync(`git init`, execOpt)
+  execSync(`git status`, execOpt)
+  execSync(`git add .`, execOpt)
+  execSync(`git status`, execOpt)
+  execSync(`git -c user.name="${process.env.GIT_USER_NAME}" -c user.email="${process.env.GIT_EMAIL}" commit -m "updates"`, execOpt)
+  execSync(`git push --set-upstream https://${process.env.GITHUB_TOKEN}@github.com/${copy}.git master`, execOpt)
+
   process.chdir(`/tmp/`)
   execSync(`rm -rf ${tempDir}`, execOpt)
 
